c8e9dbb994c52082f43fffe18776b82fe2c95671
Cyrille Derche <c.derche@me.com>
2023-04-12 17:03:29 +0200

    ux improvement

--- app/islands/CopyButton.tsx
+++ app/islands/CopyButton.tsx
@@ -1,5 +1,7 @@
 import { useState } from "preact/hooks";
 import { Button } from "../components/Button.tsx";
+import DocumentDuplicateIcon from "../components/DocumentDuplicateIcon.tsx";
+import CheckmarkIcon from "../components/CheckmarkIcon.tsx";
 
 interface CopyCodeProps {
   text: string;
@@ -19,8 +21,8 @@ export default function CopyCode(props: CopyCodeProps) {
         <div class={'bg-gray-100 rounded-lg px-4 py-1 flex gap-4 justify-between items-center'}>
             <code>{props.text}</code>
             <div class={'flex gap-1 items-center'}>
-            <Button onClick={copy}>Copy</Button>
-            { isCopied && <>✅</>}
+            <Button onClick={copy}><DocumentDuplicateIcon /></Button>
+            { isCopied && <CheckmarkIcon />}
             </div>
         </div>
   );
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -13,7 +13,8 @@ export default function Home() {
 
           <h1 className='text-4xl font-bold'>Social XP</h1>
           <h2 className='text-gray-500 text-xl'>Telegram bot for providing on-chain rewards for community members.</h2>
-          <p className='text-gray-500'>SocialXP is currently under construction 🏗️. Join our <a class={'hover:underline text-blue-600 font-semibold'} href='https://t.me/+OUQ9MU8YpJ02ZDY8'>Telegram Group</a> if you have questions.</p>
+          <p className='text-gray-500'>SocialXP is currently under construction 🏗️.</p>
+          <p className='text-gray-500'>Join our <a class={'hover:underline text-blue-600 font-semibold'} href='https://t.me/+OUQ9MU8YpJ02ZDY8'>Telegram Group</a> if you have questions.</p>
 
           <h3 class={'text-gray-900 font-bold text-2xl mt-6'}>Getting Started</h3>
           <p className='text-gray-500'>We will now configure SocialXP to work in your Telegram group.</p>
@@ -63,36 +64,36 @@ export default function Home() {
 
           <h3 class={'text-gray-900 font-bold text-2xl mt-6'}>Commands</h3>
 
-          <div class={'my-4 flex flex-col gap-2'}>
+          <div class={'my-4 flex flex-col gap-2 text-gray-500'}>
             <CopyButton text='/credit' />
             <p class={'text-gray-500 px-4'}>Shows amount of credit associated to the chat group has and a management URL to add or remove credit.</p>
           </div>
-          <div class={'my-4 flex flex-col gap-2'}>
+          <div class={'my-4 flex flex-col gap-2 text-gray-500'}>
             <CopyButton text='/connect <address>' />
             <p class={'text-gray-500 px-4'}>Associates a give address to the user that enters the command. Users can change their address maximum once a day.</p>
           </div>
 
-          <div class={'my-4 flex flex-col gap-2'}>
+          <div class={'my-4 flex flex-col gap-2 text-gray-500'}>
             <CopyButton text='/mint <amount> <user>' />
             <p class={'text-gray-500 px-4'}>Reward members at anytime by minting tokens. Note that only the <span class={'underline'}>group creator</span> is allowed to mint new tokens for their community members.</p>
           </div>
 
-          <div class={'my-4 flex flex-col gap-2'}>
+          <div class={'my-4 flex flex-col gap-2 text-gray-500'}>
             <CopyButton text='/burn <amount> <user>' />
             <p class={'text-gray-500 px-4'}>Punish members at anytime by burning their tokens. Note that only the <span class={'underline'}>group creator</span> is allowed to mint new tokens for their community members.</p>
           </div>
 
-          <div class={'my-4 flex flex-col gap-2'}>
+          <div class={'my-4 flex flex-col gap-2 text-gray-500'}>
             <CopyButton text='/rank <user>' />
             <p class={'text-gray-500 px-4'}>Displays a users rank and token holdings.</p>
           </div>
 
-          <div class={'my-4 flex flex-col gap-2'}>
+          <div class={'my-4 flex flex-col gap-2 text-gray-500'}>
             <CopyButton text='/leadership' />
             <p class={'text-gray-500 px-4'}>Lists the top 10 ranked users and their token holdings.</p>
           </div>
 
-          <div class={'my-4 flex flex-col gap-2'}>
+          <div class={'my-4 flex flex-col gap-2 text-gray-500'}>
             <CopyButton text='/me' />
             <p class={'text-gray-500 px-4'}>Displays information about the account that runs the command including rank, holdings, and associated address.</p>
           </div>
