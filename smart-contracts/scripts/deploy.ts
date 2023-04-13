8540642b49321f3a1fee3474edad44c8d0798fda
Cyrille Derche <c.derche@me.com>
2023-04-13 16:06:10 +0200

    remove Lock.sol, update deploy.ts

--- smart-contracts/scripts/deploy.ts
+++ smart-contracts/scripts/deploy.ts
@@ -1,18 +1,15 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [owner, relay, treasury] = await ethers.getSigners();
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  const Contract = await ethers.getContractFactory("SocialXP");
+  const contract = await Contract.deploy(relay.address, treasury.address)
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  await contract.deployed();
 
   console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `SocialXP deployed to ${contract.address} by ${owner.address}`
   );
 }
 
