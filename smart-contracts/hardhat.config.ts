d86dbb540a81687442c37fbdc74f6f9355a3eb1c
Cyrille Derche <c.derche@me.com>
2023-04-13 18:26:58 +0200

    deployment config

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -8,8 +8,21 @@ const config: HardhatUserConfig = {
     enabled: true,
     gasPrice: 21,
     currency: 'USD',
-    coinmarketcap: String(process.env.CMC_API_KEY)
-  }
+    coinmarketcap: String(process.env.CMC_API_KEY),
+    // token: 'MATIC',
+    // gasPriceApi: 'https://api.polygonscan.com/api?module=proxy&action=eth_gasPrice',
+    // token: 'BNB',
+    // gasPriceApi: 'https://api.bscscan.com/api?module=proxy&action=eth_gasPrice',
+  },
+  networks: {
+    sepolia: {
+      url: 'https://rpc2.sepolia.org',
+      accounts: [String(process.env.PRIVATE_KEY_OWNER), String(process.env.PRIVATE_KEY_RELAY)]
+    }
+  },
+  etherscan: {
+    apiKey: String(process.env.ETHERSCAN_API_KEY),
+  },
 };
 
 export default config;
--- smart-contracts/scripts/deploy.ts
+++ smart-contracts/scripts/deploy.ts
@@ -1,16 +1,23 @@
-import { ethers } from "hardhat";
+import { ethers, run } from "hardhat";
 
 async function main() {
-  const [owner, relay, treasury] = await ethers.getSigners();
+  const [owner, relay] = await ethers.getSigners();
 
   const Contract = await ethers.getContractFactory("SocialXP");
-  const contract = await Contract.deploy(relay.address, treasury.address)
+  const contract = await Contract.deploy(relay.address)
 
   await contract.deployed();
 
   console.log(
     `SocialXP deployed to ${contract.address} by ${owner.address}`
   );
+
+  await run("verify:verify", {
+    address: contract.address,
+    constructorArguments: [
+      relay.address
+    ],
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
